e899c5da9dedc26ac71f766734c83bc94e4958c2
nish1630 <browsing201@gmail.com>
2024-10-04 22:44:22 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(5000,()=>{console.log("App listening on port 5000...")});
+app.listen(5000, () => {
+  console.log("App listening on port 5000...");
+});
