09812509c00bec00e03feaf712ecdcb748d3e765
nish1630 <browsing201@gmail.com>
2024-10-05 18:43:23 +0530

    create a middleware function to check possible errors

--- api/index.js
+++ api/index.js
@@ -24,3 +24,14 @@ app.listen(5000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/user', userAuth);
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
