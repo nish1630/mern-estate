3b388d0af3a18c96c9f6cf1b0d26aac6c34ba5b5
nish1630 <browsing201@gmail.com>
2024-10-08 20:06:50 +0530

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/user/auth', {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers:{
           'Content-Type' : 'application/json',
