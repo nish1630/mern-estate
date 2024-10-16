b60f25781d382e813e668e3342619a517d1ba534
nish1630 <browsing201@gmail.com>
2024-10-16 15:25:16 +0530

    complete image upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
-  const { currentUser } = useSelector((state) => state.user);
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -34,18 +34,17 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/sign-in">
-            {/* <li className=" text-slate-700 hover:underline"> Sign in</li> */}
-            <Link to="/profile">
-              {currentUser ? (
+            {currentUser ? (
+              <Link to="/profile">
                 <img
                   className="rounded-full h-7 w-7 object-cover"
                   src={currentUser.avatar}
                   alt="Profile"
                 />
-              ) : (
-                <li className="text-slate-700 hover:underline">Sign in</li>
-              )}
-            </Link>
+              </Link>
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
